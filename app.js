d8abd9a347a8a1e2136d34ececf6bf6f0de08dca
hadolas <hadiyahmughal@gmail.com>
2018-03-20 13:42:08 +0000

    Add list of recipe objects to app.js; Create recipes.ejs file which loops through all recipes; Link to partials files in homepage.ejs and recipes.ejs; Connect to Bootstrap; Create .gitignore directory;

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ app.get("/", function(req, res){
 });
 
 app.get("/recipes", function(req, res){
-    res.send("RECIPES PAGE");
+    var recipes = [
+            {name: "Hazel-glaze Poppy Seed Donuts", image: "https://source.unsplash.com/68A4Vq_2biY"},
+            {name: "Oaty Biscuits", image: "https://source.unsplash.com/Py0QLFEAagY"},
+            {name: "Carrot Soup", image: "https://source.unsplash.com/w6ftFbPCs9I"},
+            {name: "Spanish Paella", image: "https://source.unsplash.com/Pt_YmiYm7a4"},
+            {name: "Spinich and Ricotta Ravioli", image: "https://source.unsplash.com/IZzch3494jg"}
+        ]
+        
+        res.render("recipes", {recipes:recipes});
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
