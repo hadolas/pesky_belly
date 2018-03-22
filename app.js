2cd27c823f26da9dca5c1cf14e2fc9a426e6c1ce
hadolas <hadiyahmughal@gmail.com>
2018-03-22 15:43:18 +0000

    Create POST route to /recipes; Install body-parser; Make 'new recipe' form; Create GET route to 'new recipe' form;

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var recipes = [
+            {title: "Hazel-glaze Poppy Seed Donuts", image: "https://source.unsplash.com/68A4Vq_2biY"},
+            {title: "Oaty Biscuits", image: "https://source.unsplash.com/Py0QLFEAagY"},
+            {title: "Carrot Soup", image: "https://source.unsplash.com/w6ftFbPCs9I"},
+            {title: "Spanish Paella", image: "https://source.unsplash.com/Pt_YmiYm7a4"},
+            {title: "Spinich and Ricotta Ravioli", image: "https://source.unsplash.com/IZzch3494jg"}
+        ]
 
 app.get("/", function(req, res){
    res.render("homepage"); 
 });
 
+// Route for rendering the "recipes" page.
 app.get("/recipes", function(req, res){
-    var recipes = [
-            {name: "Hazel-glaze Poppy Seed Donuts", image: "https://source.unsplash.com/68A4Vq_2biY"},
-            {name: "Oaty Biscuits", image: "https://source.unsplash.com/Py0QLFEAagY"},
-            {name: "Carrot Soup", image: "https://source.unsplash.com/w6ftFbPCs9I"},
-            {name: "Spanish Paella", image: "https://source.unsplash.com/Pt_YmiYm7a4"},
-            {name: "Spinich and Ricotta Ravioli", image: "https://source.unsplash.com/IZzch3494jg"}
-        ]
-        
         res.render("recipes", {recipes:recipes});
+});
+
+// Route 
+app.post("/recipes", function(req, res){
+    // get data from "create recipe" form
+    //res.send(req.body);
+    var recipe_title = req.body.recipe_title;
+    var recipe_image = req.body.recipe_image;
+    var newRecipe = {title: recipe_title, image: recipe_image}
+    recipes.push(newRecipe);
+    // redirect to "recipes" page
+    res.redirect("/recipes");
+});
+
+// Route to 'create new recipe' form
+app.get("/recipes/new", function(req, res){
+   res.render("new_recipe"); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
