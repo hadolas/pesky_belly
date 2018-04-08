96c8c271d39e915b9be297e65bc02311d1765054
hadolas <hadiyahmughal@gmail.com>
2018-04-08 13:32:18 +0000

    Connect to MongoDB using mongoose; Define Recipe schema; Create Recipe model; Create CREATE route; Create SHOW route;

--- app.js
+++ app.js
@@ -1,42 +1,81 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/pesky_belly");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var recipes = [
-            {title: "Hazel-glaze Poppy Seed Donuts", image: "https://source.unsplash.com/68A4Vq_2biY"},
-            {title: "Oaty Biscuits", image: "https://source.unsplash.com/Py0QLFEAagY"},
-            {title: "Carrot Soup", image: "https://source.unsplash.com/w6ftFbPCs9I"},
-            {title: "Spanish Paella", image: "https://source.unsplash.com/Pt_YmiYm7a4"},
-            {title: "Spinich and Ricotta Ravioli", image: "https://source.unsplash.com/IZzch3494jg"}
-        ]
+//RECIPE SCHEMA
+var recipeSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    desciption: String
+});
+//RECIPE MODEL
+var Recipe = mongoose.model("Recipe", recipeSchema);
 
+//ADD RECIPE TO DATABASE
+// Recipe.create({
+//     title: "Chicken and Onion Burger", 
+//     image: "https://source.unsplash.com/pLKgCsBOiw4", 
+//     desciption: "Try this chicken and onion burger yum"
+// }, function(err, recipe){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log("NEW RECIPE\n", recipe);
+//     }
+// });
+
+//ROUTE: RENDER HOMEPAGE
 app.get("/", function(req, res){
    res.render("homepage"); 
 });
 
-// Route for rendering the "recipes" page.
+//INDEX ROUTE: Show all recipes
 app.get("/recipes", function(req, res){
-        res.render("recipes", {recipes:recipes});
+    Recipe.find({}, function(err, recipe_results){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index_recipes", {recipes:recipe_results});  
+        }
+    })    
 });
 
-// Route 
+//CREATE ROUTE: Add recipe to database
 app.post("/recipes", function(req, res){
     // get data from "create recipe" form
     //res.send(req.body);
     var recipe_title = req.body.recipe_title;
     var recipe_image = req.body.recipe_image;
     var newRecipe = {title: recipe_title, image: recipe_image}
-    recipes.push(newRecipe);
-    // redirect to "recipes" page
-    res.redirect("/recipes");
+    
+    //Add recipe to db
+        Recipe.create(newRecipe, function(err, new_recipe){
+            if(err){
+                console.log(err);
+            } else {
+                console.log("NEW RECIPE\n", new_recipe);
+                //REDIRECT TO "/recipes" PAGE
+                res.redirect("/index_recipes");
+            }
+        });
 });
 
-// Route to 'create new recipe' form
+//NEW - Render 'create new recipe' form
 app.get("/recipes/new", function(req, res){
    res.render("new_recipe"); 
+});
+
+//SHOW - Show one recipe (in more detail)
+app.get("/recipes/:id", function(req, res){
+    //req.findById();
+    //res.send("This is the show page for this recipe :)");
+    res.render("show");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
