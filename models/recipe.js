6446c173badde1fd4652675515a4f8eca4087781
hadolas <hadiyahmughal@gmail.com>
2018-04-16 11:23:04 +0000

    Add notes attribute (as a list, and obj id ref) to Recipe Schema

--- models/recipe.js
+++ models/recipe.js
@@ -4,7 +4,13 @@ var mongoose = require("mongoose");
 var recipeSchema = new mongoose.Schema({
     title: String,
     image: String,
-    description: String
+    description: String,
+    notes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Note"
+        }
+    ]
 });
 //RECIPE MODEL
 var Recipe = mongoose.model("Recipe", recipeSchema);
